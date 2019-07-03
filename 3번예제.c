28da72aeaf74ef96c27ae7511513b7bef8131905
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-03 11:07:39 +0900

    Update 3번예제.c

--- 3번예제.c
+++ 3번예제.c
@@ -26,70 +26,149 @@ int main()
     while(1)
     {
       puts("각 메뉴는 순차적으로 한 번씩 실행됩니다.");
-      printf("%d번 메뉴를 실행하세요\n",m);
       puts("1. 네 명의 정보 입력하기");
       puts("2. 입력 된 사람의 정보 삭제");
       puts("3. 입력 된 사람의 정보 뒤에 새로운 사람 정보 삽입");
       puts("4. 입력 된 사람의 정보 뒤에 있는 사람 정보 삭제");
       puts("5. 마지막 노드가 처음을 가리키게 함"); //환상형 연결리스트
       puts("6. 종료");
+      
+      printf("\n%d번 메뉴를 실행하세요\n",m);
       scanf("%d",&c);
       getchar();
       
-      if(c!=m)
-        printf("순서대로 입력해 주세요. 현재 %d 번",m);
-     else
-     {
-       m++;
-       break;
-     }
+         if(c!=m)
+            printf("순서대로 입력해 주세요. 현재 %d 번",m);
+         else
+            {
+              m++;
+              break;
+             }
       
-      switch(c)
-      {
-          case 1;
-          while(cnt !=4)
-          {
-            p=(NODE *)malloc(sizeof(NODE));
-            if(p==null)
-            {
-              puts("메모리 할당 오류");
-              exit(1);
-            }
-            cnt++;
-            printf("이름을 입력하세요 : ");
-            gets(buffer);
-            strcpy(p -> name, buffer);
+         switch(c)
+              {
+                  case 1;
+                  while(cnt !=4)
+                     {
+                          p=(NODE *)malloc(sizeof(NODE));
+                          if(p==null)
+                              {
+                                 puts("메모리 할당 오류");
+                                 exit(1);
+                               }
+                           cnt++;
+                           printf("이름을 입력하세요 : ");
+                           gets(buffer);
+                           strcpy(p -> name, buffer);
             
-            if(h==null)
-            {
-              h=p;
-              mid=p;
-            }
-            else
-            {
-              mid -> link=p;
-              mid=p;
-            }
-            p -> link=null;
-          }
-          printf("\n물리적 저장 구조를 알아보기 위한 주소 출력\n");
-          p=h;
-          while(p!=null)
-          {
-            printf("%10s ->",p->name);
-            p=p->link;
-          }
-          printf("null\n");
-          p=h;
-          while(p!=null)
-          {
-            printf("%10d ->",p);
-            p= p -> link;
-          }
-          printf("null\n");
-          break;
+                          if(h==null)
+                            {
+                              h=p;
+                              mid=p;
+                            }
+                          else
+                           {
+                              mid -> link=p;
+                              mid=p;
+                           }
+                           p -> link=null;
+                      }
+                   printf("\n물리적 저장 구조를 알아보기 위한 주소 출력\n");
+                   p=h;
+                  while(p!=null)
+                     {
+                       printf("%10s ->",p->name);
+                       p=p->link;
+                      }
+                   printf("null\n");
+                   p=h;
+                   while(p!=null)
+                     {
+                        printf("%10d ->",p);
+                        p= p -> link;
+                      }
+                   printf("null\n");
+                   break;
           
-          case 2;
+              case 2;
+                   printf("이름을 입력하세요 : ");
+                   gets(buffer);
+          
+                   p=h;
+                  while(p!=null)
+                    {
+                      if(strcmp(p -> name,buffer)==0)
+                      break;
+                      else
+                      p=p->link;
+                     }
+                   if(p==null)
+                   puts("일치하는 사람이 없습니다.");
+                  else
+                   {
+                      if(p==h)
+                        {
+                           h=p->link;
+                           free(p);
+                         }
+                      else if(p->link==null)
+                         {
+                           mid=p;
+                           p=h;
+                           while(p->link !=mid)
+                             {
+                                 p=p->link;
+                              }
+                           p->link=null;
+                           free(mid);
+                          }
+                        else
+                         {
+                           mid=p;
+                           p=h;
+                           while(p->link !=mid)
+                             {
+                                  p=p->link;
+                             }
+                           p->link=mid->link;
+                           free(mid);
+                          }
+                    }
+                    printf("\n삭제 이후의 연결리스트 모습\n");
+                    p=h;
+                    while(p!=null)
+                    {
+                      printf("%10s ->",p->name);
+                      p=p->link;
+                    }
+                    printf("null\n");
+                    p=h;
+                    while(p!=null)
+                    {
+                      printf("%10d->",p);
+                      p=p->link;
+                    }
+                    printf("null\n");
+                    break;
+             case 3;
+                temp=(NODE*)malloc(sizeof(NODE));
+                if(temp==null)
+                {
+                  puts("메모리 할당 오류");
+                  exit(1);
+                }
+                printf("삽입할 사람의 이름을 입력하세요 :");
+                gets(temp->name);
+                printf("누구의 뒤에 삽입 하겠습니까? :");
+                gets(buffer);
+                p=h;
+                while(p!=null)
+                {
+                  
+             
+                             
+             
+              
           
                    
          
